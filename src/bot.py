08371b23dfef7187ae1ec901262eda2511e96184
haiweipc <3525843539@qq.com>
2026-01-17 18:44:35 +0800

    修改了prompt

--- src/bot.py
+++ src/bot.py
@@ -64,9 +64,9 @@ class ChatBotSession:
                 await asyncio.sleep(0.1)
             else:
                 msg = await self.message_stream.get_new_message()
-                template_msg = f"""QQ铃声的振动引起了你的注意，看到了这个群聊的天聊天记录如下
+                template_msg = f"""QQ铃声的振动引起了你的注意，看到了这个群聊的天聊  天记录如下
 {msg}
-你发送消息说（不要输出多余的内容！）："""
+你发送消息说（禁止出现动作描述）："""
                 try:
                     response = await UseAPI(current_uesrmsg=template_msg,global_cfg=self.cfg,llm_role=self.cfg.get("setup","setting"))
                     await self.send_text_message(
